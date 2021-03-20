4a2d5e7733d71554dced55641e6547a784f8af83
Simon Žižek <spymon12@gmail.com>
2021-03-20 17:35:47 +0100

    add set git-areas name to cards, add config to create media mixins and responsive to add styles for different screen sizes, add tilt.js to animate cards on hover

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('[data-tilt]').tilt({
+    maxTilt: 20,
+    perspective: 500, // Transform perspective, the lower the more extreme the tilt gets.
+    easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
+    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+    speed: 300, // Speed of the enter/exit transition.
+    transition: true, // Set a transition on enter/exit.
+    disableAxis: null, // What axis should be disabled. Can be X or Y.
+    reset: true, // If the tilt effect has to be reset on exit.
+    glare: false, // Enables glare effect
+    maxGlare: 1, // From 0 - 1.
+  })
+})
